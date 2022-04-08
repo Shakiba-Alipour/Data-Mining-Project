fc2ebe7ecd28b76349674eefcc6236ae68344476
Shakiba <alipour.sh79@gmail.com>
2022-04-08 10:46:40 +0430

    get_departments is added

--- UGent.py
+++ UGent.py
@@ -3,91 +3,94 @@ import requests
 from bs4 import BeautifulSoup
 import BaseCrawler
 
-outcomes = []
-prerequisites = []
-courseName = []
-description = []
-links = []
-courses = []
 
-# response = requests.get('https://www.rug.nl/ocasys/rug/main/browseByFaculty')
 
-# soup = BeautifulSoup(response.content, 'html.parser')
+class UGhent(BaseCrawler):
+    outcomes = []
+    prerequisites = []
+    courseName = []
+    description = []
+    departments_links = []
+    courses = []
 
-# subjects_element = soup.find(class_="main") 
+    # response = requests.get('https://www.rug.nl/ocasys/rug/main/browseByFaculty')
 
-# subjects = subjects_element.find_all('li')
+    # soup = BeautifulSoup(response.content, 'html.parser')
 
-# for subject in subjects:
-#     link = 'https://www.rug.nl/ocasys/rug' + subject.find('a', id="nodeNotSelected").get('href')[2:]
-#     res = requests.get(link)
-#     soup = BeautifulSoup(res.content, 'html.parser')
-#     courses_element = soup.find(class_="userTable")
-#     courses_name = courses_element.find_all('td')
+    # subjects_element = soup.find(class_="main") 
 
-#     for course in courses_name:
-#         courseLink = course.find('a')
+    # subjects = subjects_element.find_all('li')
+
+    # for subject in subjects:
+    #     link = 'https://www.rug.nl/ocasys/rug' + subject.find('a', id="nodeNotSelected").get('href')[2:]
+    #     res = requests.get(link)
+    #     soup = BeautifulSoup(res.content, 'html.parser')
+    #     courses_element = soup.find(class_="userTable")
+    #     courses_name = courses_element.find_all('td')
+
+    #     for course in courses_name:
+    #         courseLink = course.find('a')
+            
+    #         if courseLink != None:
+    #             course_Link = 'https://www.rug.nl' + course.find('a').get('href')
+    #             course_res = requests.get(course_Link)
+    #             soup = BeautifulSoup(course_res.content, 'html.parser')
+    #             detail = soup.find(class_="detailTable")
+    #             details_name = detail.find_all('tr')
+
+    #             for detail_name in details_name:
+    #                 data = detail_name.find('td', class_='fieldLabel')                  # find title of datas 
+                    
+    #                 if data != None:
+    #                     if data.text == 'Leerdoelen':                                   # find Learning outcomes
+    #                         outcomes.append(data.findNext('td').text.strip())           # Add datas to outcomes array 
+
+    #                     if data.text == 'Entreevoorwaarden':                            # find Prerequisites
+    #                         prerequisites.append(data.findNext('td').text.strip())      # Add datas to Prerequisites array 
+                            
+    #                     if data.text == 'Uitgebreide vaknaam':                          # find course name
+    #                         courseName.append(data.findNext('td').text.strip())         # Add datas to courseName array 
+                            
+    #                     if data.text == 'Omschrijving':                                 # find overview
+    #                         description.append(data.findNext('td').text.strip())        # Add datas to description array 
+
+    #                     print(data.findNext('td').text.strip())
+
+
+    response = requests.get('https://www.ugent.be/doctoralschools/en/doctoraltraining/courses')
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    #find departments
+    def get_departments(self, soup):
+        department_element = soup.find_all('ul', id = 'parent-fieldname-text')
+        departments = department_element.find_all('a')
+
+        for department in departments:
+            link = department.get('href')
+            if link.endswith('.htm') is False:
+                self.departments_links.append(link)
+
+            
+    #   find course links
         
-#         if courseLink != None:
-#             course_Link = 'https://www.rug.nl' + course.find('a').get('href')
-#             course_res = requests.get(course_Link)
-#             soup = BeautifulSoup(course_res.content, 'html.parser')
-#             detail = soup.find(class_="detailTable")
-#             details_name = detail.find_all('tr')
+    response = requests.get(departments_links[0])
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find_all('tr')
+    for row in table:
+        if row.find('a') is not None:
+            courses.append(row.find('a').get('href'))
 
-#             for detail_name in details_name:
-#                 data = detail_name.find('td', class_='fieldLabel')                  # find title of datas 
-                
-#                 if data != None:
-#                     if data.text == 'Leerdoelen':                                   # find Learning outcomes
-#                         outcomes.append(data.findNext('td').text.strip())           # Add datas to outcomes array 
+    #   find informations of course
 
-#                     if data.text == 'Entreevoorwaarden':                            # find Prerequisites
-#                         prerequisites.append(data.findNext('td').text.strip())      # Add datas to Prerequisites array 
-                        
-#                     if data.text == 'Uitgebreide vaknaam':                          # find course name
-#                         courseName.append(data.findNext('td').text.strip())         # Add datas to courseName array 
-                        
-#                     if data.text == 'Omschrijving':                                 # find overview
-#                         description.append(data.findNext('td').text.strip())        # Add datas to description array 
-
-#                     print(data.findNext('td').text.strip())
-
-
-response = requests.get('https://www.ugent.be/doctoralschools/en/doctoraltraining/courses')
-soup = BeautifulSoup(response.content, 'html.parser')
-
-#    find departments
-def get_departments(soup):
-    department_element = soup.find_all('ul', id = 'parent-fieldname-text')
-    departments = department_element.find_all('a')
-
-    for department in departments:
-        link = department.get('href')
-        if link.endswith('.htm') is False:
-            links.append(link)
-
-        
-#   find course links
-    
-response = requests.get(links[0])
-soup = BeautifulSoup(response.content, 'html.parser')
-table = soup.find_all('tr')
-for row in table:
-    if row.find('a') is not None:
-        courses.append(row.find('a').get('href'))
-
-#   find informations of course
-
-response = requests.get('https://www.ugent.be/doctoralschools/en/doctoraltraining/courses/specialistcourses/ahl/demystifying-chan-buddhism.htm')
-soup = BeautifulSoup(response.content, 'html.parser')
-things = soup.find_all('h2')
-for thing in things:
-    if thing.text == 'Objectives':
-        objective = thing.next_element.next_element.next_element
-        print(objective)
-    else:
-        if thing.text == 'Topic and Objectives':
+    response = requests.get('https://www.ugent.be/doctoralschools/en/doctoraltraining/courses/specialistcourses/ahl/demystifying-chan-buddhism.htm')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    things = soup.find_all('h2')
+    for thing in things:
+        if thing.text == 'Objectives':
             objective = thing.next_element.next_element.next_element
             print(objective)
-#print(things)
+        else:
+            if thing.text == 'Topic and Objectives':
+                objective = thing.next_element.next_element.next_element
+                print(objective)
+    #print(things)
